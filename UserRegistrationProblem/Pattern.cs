9710bfe62c3b290ba6324d8234da023b9efd5c53
Poojachakor <chakorpooja07@gmail.com>
2023-03-21 15:31:18 +0530

    Add: UserRegistrationProblem

---
+++ UserRegistrationProblem/Pattern.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblem
+{
+    internal class Pattern
+    {
+        public static string Regex_Rule = "^[A-Z][A-Za-z]{3,9}$";
+
+        public bool validatePinCode(string inputString)
+        {
+            return Regex.IsMatch(inputString, Regex_Rule);
+        }
+    }
+}
---
+++ UserRegistrationProblem/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace UserRegistrationProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Pattern pattern = new Pattern();
+            if (pattern.validatePinCode("Pooja"))//Valid name
+                                                   //if (pattern.validatePinCode("poojachakor"))//invalid name
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+    }
+}
