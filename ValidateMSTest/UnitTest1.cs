464567eac0cdbf631746b697f53d462c840be799
Poojachakor <chakorpooja07@gmail.com>
2023-03-31 17:01:47 +0530

    Add: UC12 file

--- ValidateMSTest/UnitTest1.cs
+++ ValidateMSTest/UnitTest1.cs
@@ -1,3 +1,4 @@
+using System.Runtime.Serialization;
 using UserRegistrationProblem;
 
 namespace ValidateMSTest
@@ -62,49 +63,69 @@ namespace ValidateMSTest
         }
         //Sad Test Cases(Test Cases Fail The Entry)
         [TestMethod]
-        public void GivenUserFistName_WhenValidate_ShouldReturnFalse()
+        public void GivenUserFistName_WhenValidate_Should_throw_Custom_InvalidFirstNameException()
         {
-            string firstName = "pooja";
-            //Act
-            bool result = user.ValidateFirstName(firstName);
-            //Assert
-            Assert.IsFalse(result);
+            try
+            {
+                string firstName = "pooja";
+                bool result = user.ValidateFirstName(firstName);
+            }
+            catch (InvalidUserDetailException e)
+            {
+                Assert.AreEqual("Invalid First Name", e.Message);
+            }
         }
         [TestMethod]
-        public void GivenUserLastName_WhenValidate_ShouldReturnFalse()
+        public void GivenUserLastName_WhenValidate_Should_throw_Custom_InvalidLastNameException()
         {
-            string lastName = "chakor";
-            //Act
-            bool result = user.ValidateFirstName(lastName);
-            //Assert
-            Assert.IsFalse(result);
+            try
+            {
+                string lastName = "chakor";
+                bool result = user.ValidateLastName(lastName);
+            }
+            catch (InvalidUserDetailException e)
+            {
+                Assert.AreEqual("Invalid Last Name", e.Message);
+            }
         }
         [TestMethod]
-        public void GivenEmailId_WhenValidate_ShouldReturnFalse()
+        public void GivenEmailId_WhenValidate_Should_throw_Custom_InvalidEmailException()
         {
-            string email = "Chakor.pooja.com";
-            //Act
-            bool result = user.ValidateEmail(email);
-            //Assert
-            Assert.IsFalse(result);
+            try
+            {
+                string email = "pooja.Chakor.com";
+                bool result = user.ValidateEmail(email);
+            }
+            catch (InvalidUserDetailException e)
+            {
+                Assert.AreEqual("Invalid Email", e.Message);
+            }
         }
         [TestMethod]
-        public void GivenMobileNumber_WhenValidate_ShouldReturnFalse()
+        public void GivenMobileNumber_WhenValidate_Should_throw_Custom_InvalidMobileException()
         {
-            string mobileNumber = "91 0740613481";
-            //Act
-            bool result = user.ValidateMobile(mobileNumber);
-            //Assert
-            Assert.IsFalse(result);
+            try
+            {
+                string mobileNumber = "91 0740613418";
+                bool result = user.ValidateMobile(mobileNumber);
+            }
+            catch (InvalidUserDetailException e)
+            {
+                Assert.AreEqual("Invalid Mobile Number", e.Message);
+            }
         }
         [TestMethod]
-        public void GivenPassword_WhenValidate_ShouldReturnFalse()
+        public void GivenPassword_WhenValidate_Should_throw_Custom_InvalidPasswordException()
         {
-            string password = "chhaku@21";
-            //Act
-            bool result = user.ValidatePassword(password);
-            //Assert
-            Assert.IsFalse(result);
+            try
+            {
+                string password = "chhaku@21";
+                bool result = user.ValidatePassword(password);
+            }
+            catch (InvalidUserDetailException e)
+            {
+                Assert.AreEqual("Invalid Password", e.Message);
+            }
         }
         [TestMethod]
         [DataRow("abc@yahoo.com")]
@@ -122,4 +143,3 @@ namespace ValidateMSTest
         }
     }
 }
-    
